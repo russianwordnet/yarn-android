d1095fc810d9a60bcb4af2843a0acbf087b33a76
Sergey Igushkin <hotkeytlt@gmail.com>
2015-12-05 16:04:53 +0300

    Added MTsarService, an API definition. Added getting processes functionality. Added navigation menu to MainActivity (still needs reworking).

--- app/src/main/java/net/russianword/android/MainActivity.kt
+++ app/src/main/java/net/russianword/android/MainActivity.kt
@@ -1,18 +1,88 @@
 package net.russianword.android
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.frameLayout
+import android.view.Gravity
+import net.russianword.android.api.MTsarService
+import net.russianword.android.api.Process
+import net.russianword.android.utils.asAsync
+import net.russianword.android.utils.getActionBarColor
+import net.russianword.android.utils.getActionBarSize
+import net.russianword.android.utils.navigationView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.drawerLayout
+import rx.lang.kotlin.onError
+import kotlin.properties.Delegates
+import android.support.design.R as RR
+import org.jetbrains.anko.appcompat.v7.toolbar as tbar
 
 const val FRAGMENT_HOLDER_ID = 1
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
-    fun ui() = frameLayout { id = FRAGMENT_HOLDER_ID }
+    private var nvNavigation: NavigationView by Delegates.notNull()
+
+    private fun ui() = drawerLayout {
+        fitsSystemWindows = true
+        verticalLayout {
+            tbar().apply {
+                lparams(height = getActionBarSize(), width = matchParent)
+                backgroundColor = getActionBarColor()
+                setSupportActionBar(this)
+                supportActionBar.setDisplayHomeAsUpEnabled(true);
+
+                ActionBarDrawerToggle(
+                        act, this@drawerLayout, this@apply,
+                        R.string.acs_drawer_open, R.string.acs_drawer_close)
+                        .let {
+                            setDrawerListener(it)
+                            it.syncState()
+                        }
+            }
+            frameLayout {
+                id = FRAGMENT_HOLDER_ID
+            }
+        }
+        nvNavigation = navigationView {
+            addHeaderView(ctx.verticalLayout {
+                imageView(R.mipmap.ic_launcher) { padding = dip(16); lparams() }
+            })
+            lparams(width = wrapContent, height = matchParent, gravity = Gravity.START)
+            setNavigationItemSelectedListener() f@{ item ->
+                item.setChecked(true)
+                this@drawerLayout.closeDrawers()
+                return@f true
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ui()
-        fragmentManager.beginTransaction().add(FRAGMENT_HOLDER_ID, HelloFragment()).commit()
+        setContentView(ui())
+        fragmentManager.beginTransaction().replace(FRAGMENT_HOLDER_ID, HelloFragment()).commit()
+        listProcesses()
+    }
+
+    private fun listProcesses() {
+        MTsarService.service.listProcesses()
+                .asAsync()
+                .onError {
+                    toast(R.string.tst_load_failed)
+                    error(it.getStackTraceString())
+                }
+                .subscribe {
+                    toast(R.string.tst_processes_loaded)
+                    setMenuItems(it)
+                }
+    }
+
+    private fun setMenuItems(processes: List<Process>) {
+        with (nvNavigation) {
+            menu.clear()
+            processes.forEach { menu.add(it.description) }
+            setCheckedItem(menu.getItem(0).itemId)
+        }
     }
 }
